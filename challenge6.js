87c87af8ed39efdc693500c754e3830914c294d1
Albaric0que <albamrus@gmail.com>
2023-12-06 15:00:22 +0100

    AdventJS challenge 6

---
+++ challenge6.js
@@ -0,0 +1,5 @@
+/* Create a maxDistance function that takes the text string movements and returns the maximum distance that the reindeer can travel in any direction:
+> = Moves to the right
+< = Moves to the left
+* = Can move forward or backward 
+Same direction or '*' adds 1 and oposite direction substract 1*/
