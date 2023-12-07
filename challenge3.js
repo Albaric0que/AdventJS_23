02130d651f17c6226937e4df8c6aa3bc10d5ba37
Albaric0que <albamrus@gmail.com>
2023-12-07 16:31:06 +0100

    Explanation added

--- challenge3.js
+++ challenge3.js
@@ -4,16 +4,18 @@ Your task is to write a function that identifies and returns the first extra ste
 
 function findNaughtyStep(original, modified) {
   if (original === modified) {
+    //Comparamos si los dos string son iguales y si lo son devolvemos un string vacio.
     return "";
   }
 
-  let differentElement = 0;
+  let differentElement = 0; // Inicializamos una variable donde almacenaremos el índice del elemento diferente.
 
   for (let i = 0; original[i] === modified[i]; i++) {
-    differentElement += 1;
+    // Creamos un bucle que recorrerá los strings mientras los indices sean
+    differentElement += 1; // iguales y por cada índice igual sumará uno a differentElement.
   }
 
-  return original.length > modified.length
-    ? original[differentElement]
-    : modified[differentElement];
-}
+  return original.length > modified.length //Devolvemos directamente el resultado de un condicional ternario por el que
+    ? original[differentElement] // si la longitud de original es mas grande devolverá el elemento diferente en
+    : modified[differentElement]; // el string original, si modified es diferente devolverá el elemento diferente en
+} // modified. Conseguimos esto gracias al índice numeral almacenado en differentElement
